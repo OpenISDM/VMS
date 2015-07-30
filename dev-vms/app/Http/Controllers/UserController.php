36a6062407b5ef510c539fe0d0392e47e2ac1280
ms0495038 <ms0495038@gmail.com>
2015-07-30 10:45:22 +0800

    test

--- dev-vms/app/Http/Controllers/UserController.php
+++ dev-vms/app/Http/Controllers/UserController.php
@@ -38,6 +38,7 @@ class UserController extends Controller
                                 'email'=> $email,
                                 'cellphone'=> $cellphone,
                                 'city'=> $city]);
+		//end
     }
     public function editProfile(Request $request)
     {
