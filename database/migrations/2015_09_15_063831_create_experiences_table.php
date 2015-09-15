46ba106c98eed55052e09b0936d737dc6db049fc
YMHuang <ym.huang0808@gmail.com>
2015-09-15 16:20:48 +0800

    Correct experience migration style

--- database/migrations/2015_09_15_063831_create_experiences_table.php
+++ database/migrations/2015_09_15_063831_create_experiences_table.php
@@ -12,7 +12,7 @@ class CreateExperiencesTable extends Migration
      */
     public function up()
     {
-        Schema::create('experiences', function (Blueprint $table){
+        Schema::create('experiences', function (Blueprint $table) {
             $table->increments('id');
             $table->string('company');
             $table->string('job_title', 100);
