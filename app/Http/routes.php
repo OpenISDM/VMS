4f4297030e7f0f1a0896360d4f9b17a260c3bd07
YMHuang <ym.huang0808@gmail.com>
2015-10-04 21:58:53 +0800

    Remove tracked vender/

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,7 +32,7 @@ $api->version('v1.0', function ($api) {
     $api->group(['middleware' => 'check.header'], function ($api) {
         $api->post('register', 'App\Http\Controllers\Api\V1_0\VolunteerAuthController@register');
         $api->post('auth', 'App\Http\Controllers\Api\V1_0\VolunteerAuthController@login');
-        $api->get('email_verification/{email_address}/{verification_code}', 
+        $api->get('email_verification/{email_address}/{verification_code}',
                   'App\Http\Controllers\Api\V1_0\VolunteerAuthController@emailVerification');
     });
 });
