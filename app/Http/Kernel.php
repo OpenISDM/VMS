fd41abcf1a96104e431a4fc7c15f1579fe090268
YMHuang <ym.huang0808@gmail.com>
2015-10-04 20:55:01 +0800

    Update .travis.yml

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,5 +31,7 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'check.header' => \App\Http\Middleware\CheckHeaderFieldsMiddleware::class,
+        'jwt.auth' => Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+        'jwt.refresh' => Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 }
