9b5956bf044d7582ebe46fa9713780b7957d6b16
YMHuang <ym.huang0808@gmail.com>
2015-10-30 15:06:08 +0800

    Fix coding style

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -75,7 +75,7 @@ $api->version('v1.0', function ($api) {
         $api->get('email_verification/{email_address}/{verification_code}',
             'App\Http\Controllers\Api\V1_0\VolunteerAuthController@emailVerification');
 
-        $api->post('/resend_email_verification', 
+        $api->post('/resend_email_verification',
                    'App\Http\Controllers\Api\V1_0\VolunteerAuthController@resendEmailVerification');
 
         // Retrive volunteer's profile
