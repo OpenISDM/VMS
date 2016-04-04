978bacdd3be8e7a165339e3489eaa03a9be85c48
YMHuang <ym.huang0808@gmail.com>
2016-04-04 17:09:08 +0800

    Add ArrayUtil::combinedArray()

--- app/Utils/ArrayUtil.php
+++ app/Utils/ArrayUtil.php
@@ -21,4 +21,29 @@ class ArrayUtil
 
         return $list;
     }
+
+    public static function combinedArray(array $data, $keyName, $combinedAsKey, $combinedKeys)
+    {
+        $combinedArray = [];
+        $collection = collect($data);
+
+        $collection->each(function ($value) use (&$combinedArray, $keyName, $combinedAsKey, $combinedKeys) {
+            $combined = array_only($value, $combinedKeys);
+
+            if (isset($combinedArray[$value[$keyName]])) {
+                if (empty($combinedArray[$value[$keyName]][$combinedAsKey])) {
+                    $combinedArray[$value[$keyName]][$combinedAsKey][] = $combined;
+                } else {
+                    $combinedArray[$value[$keyName]][$combinedAsKey][] = $combined;
+                }
+            } else {
+                $origin = array_except($value, $combinedKeys);
+
+                $combinedArray[$value[$keyName]] = $origin;
+                $combinedArray[$value[$keyName]][$combinedAsKey][] = $combined;
+            }
+        });
+
+        return $combinedArray;
+    }
 }
