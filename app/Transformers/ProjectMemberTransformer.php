fb8682a7da007905457c650954190902f7514a43
YMHuang <ym.huang0808@gmail.com>
2016-06-07 11:18:04 +0800

    Add ProjectMemberTransformer
    Add email field in

---
+++ app/Transformers/ProjectMemberTransformer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use App\Volunteer;
+
+class ProjectMemberTransformer extends TransformerAbstract
+{
+    public function transform(Volunteer $user)
+    {
+        $visibleFields = [
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'avatar_path'
+        ];
+        $userArray = $user->toArray();
+        $item = array_only($userArray, $visibleFields);
+
+        return $item;
+    }
+}
